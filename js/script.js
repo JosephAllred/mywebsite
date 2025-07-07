1f8da0e9d83dd5986736dd381eb85e4f70e46e4c
Joseph A <126510531+JosephAllred@users.noreply.github.com>
2025-07-07 09:10:24 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,25 @@
+// Get search input 
+
+const search = document.getElementById('search');
+
+search.addEventListener('keyup', function() {
+    // Get input value
+    const term = search.value; 
+    // Filter logic
+    // Display matched posts
+    // Hide others
+})
+
+// Get form element
+const form = document.getElementById('newsletter-form');
+form.addEventListener('submit', function(e) {
+    // prevent page refresh
+    e.preventDefault();
+
+    // grab input value
+    const email = form.email.value; 
+
+    // Submit to API
+    // Show success message
+})
+
